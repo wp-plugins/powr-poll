c783afc57ba46df46dcbcc655fc716d3cd0c5005
powr <powr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-11 06:47:58 +0000

    App and readme updates
    git-svn-id: https://plugins.svn.wordpress.org/powr-poll/trunk@987002 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- powr-poll.php
+++ powr-poll.php
@@ -1,14 +1,14 @@
 <?php
     /**
      * @package POWr Poll
-     * @version 1.0
+     * @version 1.1
      */
     /*
     Plugin Name: POWr Poll
     Plugin URI: http://www.powr.io
     Description: Get to know your users with a customized Poll! Add the widget to your theme, or create a Poll on ANY page or post by using the shortcode [powr-poll]. Then, simply visit your site and click the settings icon to customize your Poll right in the page. Many more plugins & tutorials at POWr.io.
     Author: POWr.io
-    Version: 1.0
+    Version: 1.1
     Author URI: http://www.powr.io
     */
 
@@ -42,7 +42,7 @@
           if(powr_local_mode()){//Determine JS url:
             $js_url = '//localhost:3000/powr_local.js';
           }else{
-            $js_url = '//powr.io/powr.js';
+            $js_url = '//www.powr.io/powr.js';
           }
           ?>
           <script>
